d5116adeccf5877210039debe3bc7f9ade9c8653
troquatte <d.troquatte20@yahoo.com.br>
2025-03-31 22:38:28 +0100

    Configurações iniciais

---
+++ docs/chunk-WR7JNEDZ.js
@@ -0,0 +1,1 @@
+import{Ma as o,ab as a,bb as p,kb as i}from"./chunk-VJARIFHF.js";var n=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-not-found"]],decls:2,vars:0,template:function(e,m){e&1&&(a(0,"p"),i(1,"P\xE1gina n\xE3o encontrada!"),p())},encapsulation:2})};export{n as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,10 @@ export const routes: Routes = [
     path: ':menu/:subMenu',
     loadComponent: () => import('./shared/pages/content/content.component'),
   },
+  {
+    path: '404',
+    title: 'Página não encontrada',
+    loadComponent: () => import('./shared/pages/not-found/not-found.component'),
+  },
+  { path: '**', pathMatch: 'full', redirectTo: '404' },
 ];
